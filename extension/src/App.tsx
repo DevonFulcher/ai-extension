afe972e36ecd659260b33598aaffe0d4c008dafb
Devon Fulcher <devonfulcher3@gmail.com>
2023-04-19 21:30:54 -0500

    message passing is working

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
+    console.log(document.getElementsByTagName("article"))
+    const message = { type: 'FETCH_DATA' };
+    /*
     fetch('https://swapi.dev/api/films/1/')
       .then(response => response.json())
       .then(data => console.log(data))
       .catch(error => console.error(error));
+    */
   }
+
+  (async () => {
+    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+    if (tab.id == undefined) {
+      return
+    }
+    const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
+    // do something with response here, not outside the function
+    console.log(response);
+  })();
 
   return (
     <div>
